c8bfa7263c21b22306280d565d334351dc8dbeca
Evgenii <forsimen@gmail.com>
2018-08-01 16:51:31 +0200

    Added forces acquisition to main.m

--- main.m
+++ main.m
@@ -1,7 +1,9 @@
 p = gcp(); % get the current parallel pool 
 
 get_tcp_coordinates = parfeval(p, @ur5_client, 1, '192.168.56.102', 30003); 
-get_tcp_coordinates_clone = parfeval(p, @ur5_client, 1, '192.168.56.101', 30003); 
+% get_tcp_coordinates_clone = parfeval(p, @ur5_client, 1, '192.168.56.101', 30003); 
+get_forces = parfeval(p, @daq_client, 1);
 
 tcp_coordinates = fetchOutputs(get_tcp_coordinates); % Blocks until complete
-tcp_coordinates_clone = fetchOutputs(get_tcp_coordinates_clone); % Blocks until complete
+forces = fetchOutputs(get_forces); % Blocks until complete
+% tcp_coordinates_clone = fetchOutputs(get_tcp_coordinates_clone); % Blocks until complete
